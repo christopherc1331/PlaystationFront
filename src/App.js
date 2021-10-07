c4d75dbcfc6ac66697c0cdf6d23ae830aa2f40fc
Christopher Cruz <christopherc1331@gmail.com>
2021-10-07 01:24:07 -0700

    Got basic design of pop up card done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
     <div className="container">
+      <div className="pop-up-box">
+        <div className="header">
+            <h1>Feature Flag Manager</h1>
+            <div className="white-rect"/>
+        </div>
+        <div className="box-bottom">
+            <div className="bottom-inner-box">
 
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
