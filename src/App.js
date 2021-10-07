1ddcf07ac46e41478cb1af4c346094c81c801e7a
Christopher Cruz <christopherc1331@gmail.com>
2021-10-07 01:50:04 -0700

    Got header set up

--- src/App.js
+++ src/App.js
@@ -10,7 +10,29 @@ function App() {
         </div>
         <div className="box-bottom">
             <div className="bottom-inner-box">
+                <div className="empty-row">
 
+                </div>
+                <div className="header-row">
+                    <div className="row-container">
+                        <div className="left">
+                            <h3>Region</h3>
+                        </div>
+                        <div className="right">
+                            <h3>Asia</h3>
+                            <h3>Korea</h3>
+                            <h3>Europe</h3>
+                            <h3>Japan</h3>
+                            <h3>America</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="check-box-row">
+
+                </div>
+                <div className="button-row">
+
+                </div>
             </div>
         </div>
       </div>
